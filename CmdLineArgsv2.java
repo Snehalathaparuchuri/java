222cdec54bce85661731eeb5be319af76c8c9837
Snehalathaparuchuri <143867323+Snehalathaparuchuri@users.noreply.github.com>
2023-09-09 12:18:06 +0530

    Add files via upload

---
+++ CmdLineArgsv2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class CmdLineArgsv2 
+{
+  public static void main(String[] args)
+
+      if(args.length!=2)
+      {
+          System.out.println("Invalid no of arguments");
+      }
+      else
+      {
+         int x=Integer.parseInt(args[0]);
+         int y=Integer.parseInt(args[1]);
+         int z=x+y;
+         System.out.println(" sum of " +x+ " and " +y+ " is " +z);
+      }
+  }
+}
+  
