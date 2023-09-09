7a77e9a4f2ea46316d29cb18164a3ec116225b4d
Snehalathaparuchuri <143867323+Snehalathaparuchuri@users.noreply.github.com>
2023-09-09 09:20:17 +0530

    Create DefaultDemo.java

---
+++ DefaultDemo.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Sample
+{
+   void display()
+   {
+      System.out.println("HELLO WORLD");
+   }
+}
+public class DefaultDemo
+{
+    public static void main(String[] args)
+    {
+        Sample s = new Sample();
+        s.display();
+    }
+}
